37f1cbcc2baff56df5125798d729a92b9747c68b
Robert Comstock <rlc@game.rlc3.com>
2025-08-16 08:02:19 -0600

    Added count

---
+++ count.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+quake=$(quake_count.sh)
+mumble=$(mumble_count.sh)
+minecraft=$(minecraft_count.sh)
+
+i=0
+
+if [ "$quake" -eq 0 ]; then
+	echo "No quake users"
+else
+	echo "Current quake users"
+	i=$((i+1))
+fi
+
+if [ "$mumble" -eq 0 ]; then
+	echo "No mumble users"
+else
+	echo "Current mumble users"
+	i=$((i+1))
+fi
+
+if [ "$minecraft" -eq 0 ]; then
+	echo "No minecraft users"
+else
+	echo "Current minecraft users"
+	i=$((i+1))
+fi
+
+if [ "$i" -eq 0 ]; then
+	echo "No game users"
+else
+	echo "There are current game users"
+fi
